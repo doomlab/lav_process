bf9e0f67cb030734089d6264510eaabe7ac47f7f
Sam Mancuso <mancusos@unimelb.edu.au>
2020-02-19 11:52:04 +1100

    Added seed and support for categorical variables, multiple mediators, and robust standard errors.
    Removed printing of model to screen.

--- R/lavaan_models.R
+++ R/lavaan_models.R
@@ -3,7 +3,10 @@ require(lavaan)
 
 lavaan.model <- function(y,
                          x,
+                         # Mediators
                          m,
+                         # Categorical variables
+                         categorical = NULL,
                          # Covariates
                          cv = NULL,
                          # Covariates for Y ~ X (and Y ~ M)
@@ -11,12 +14,22 @@ lavaan.model <- function(y,
                          # Covariates for M ~ X
                          cv.mx = FALSE,
                          data,
-                         # Number of boostrap resamples
+                         # Test statistic
+                         # Robust variants:
+                         # "Satorra-Bentler" and "Yuan-Bentler"
+                         test = "standard",
+                         # Number of bootstrap resamples
                          B = 5000,
+                         seed = 1234,
                          process.model = 4) {
+  # Set Seed
+  set.seed(seed)
 
   # PROCESS Model 4 (Single Mediator)
   if (process.model == 4) {
+
+    # Calculate SD of Y for partially standardized effect size
+    sd.y <- sd(data[, y], na.rm = TRUE)
 
     # Direct effect
     # Y ~ X
@@ -28,38 +41,135 @@ lavaan.model <- function(y,
       # Y ~ c*X
       paste0(y, "~", "c*", x)
     )
-    # Mediator
-    # M ~ X
-    mediator.mx <- ifelse(
-      # Covariates for M ~ X
-      !is.null(cv) & isTRUE(cv.mx),
-      # M ~ a*X + CV1 + ... CVn
-      paste0(m, "~", "a*", x, "+", paste0(cv, collapse = "+")),
-      # M ~ a*X
-      paste0(m, "~", "a*", x)
-    )
 
-    # Y ~ M
-    mediator.ym <- paste0(y, "~", "b*", m)
+    # Mediator(s)
+    # Check length of m
+    if (length(m) == 1) {
 
-    # Indirect effect
-    indirect.eff <- "ab := a*b"
-    # Total effect
-    total.eff <- "total := c + (a*b)"
+      # Mediator
+      # M ~ X
+      mediator.mx <- ifelse(
+        # Covariates for M ~ X
+        !is.null(cv) & isTRUE(cv.mx),
+        # M ~ a*X + CV1 + ... CVn
+        paste0(m, "~", "a*", x, "+", paste0(cv, collapse = "+")),
+        # M ~ a*X
+        paste0(m, "~", "a*", x)
+      )
 
-    # Specify model
-    model <- paste(
-      direct.eff,
-      mediator.mx,
-      mediator.ym,
-      indirect.eff,
-      total.eff,
-      sep = "\n"
-    )
+      # Y ~ M
+      mediator.ym <- paste0(y, "~", "b*", m)
+
+      # Indirect effect
+      indirect.eff <- "ab := a*b"
+      # Total effect
+      total.eff <- "total_effect := c + (a*b)"
+
+      # Partially standardized indirect effect size
+      a.es <- paste0("ab_es := ab/", sd.y)
+
+      # Specify model
+      model <- paste(
+        direct.eff,
+        mediator.mx,
+        mediator.ym,
+        indirect.eff,
+        total.eff,
+        a.es,
+        sep = "\n"
+      )
+    } else {
+
+      # Declare variables for loop
+      mediator.mx <- vector("character", length = length(m))
+      indirect.eff <- vector("character", length = length(m))
+      mediator.ym <- vector("character", length = length(m))
+      total.eff <- vector("character", length = length(m))
+      indirect.es <- vector("character", length = length(m))
+
+      # Loop through mediators
+      for (i in 1:length(m)) {
+
+        # Covariates have been selected for M ~ X
+        if (!is.null(cv) & isTRUE(cv.mx)) {
+          mediator.mx[i] <- paste0(
+            m[i], "~", "a", i, "*", x, "+", paste0(cv, collapse = "+")
+          )
+        } else {
+          mediator.mx[i] <- paste0(
+            m[i], "~", "a", i, "*", x
+          )
+        }
+
+        # Y ~ M
+        mediator.ym[i] <- paste0(y, "~", "b", i, "*", m[i])
+
+        # Indirect effect
+        indirect.eff[i] <- paste0("a", i, "b", i, ":=", "a", i, "*", "b", i)
+
+        # Total effect
+        total.eff[i] <- paste0("(", "a", i, "*", "b", i, ")")
+
+        # Partially standardized indirect effect size
+        indirect.es[i] <- paste0(
+          paste0("a", i, "b", i, "_es:=", "(a", i, "*", "b", i, ")/", sd.y)
+        )
+      }
+
+      # Collapse variables
+      mediator.mx <- paste0(mediator.mx, collapse = "\n")
+      mediator.ym <- paste0(mediator.ym, collapse = "\n")
+      indirect.eff <- paste0(indirect.eff, collapse = "\n")
+      indirect.es <- paste0(indirect.es, collapse = "\n")
+
+      # Total indirect effect
+      total.ind.eff <- paste0(
+        "total_indirect:=", paste0(total.eff, collapse = "+")
+      )
+
+      # Partially standardized total indirect effect
+      total.ind.eff.es <- paste0(
+        "total_indirect_es:=", "total_indirect/", sd.y
+      )
+
+      # Total direct effect
+      total.eff <- paste0("total_effect:=c+", paste0(total.eff, collapse = "+"))
+
+      # Specify model
+      model <- paste(
+        direct.eff,
+        mediator.mx,
+        mediator.ym,
+        indirect.eff,
+        total.ind.eff,
+        total.eff,
+        indirect.es,
+        total.ind.eff.es,
+        sep = "\n"
+      )
+    }
 
     # Run mediation analyses
-    fit <- sem(model, data = data, se = "bootstrap", bootstrap = B)
-
+    # Categorical variables
+    if (!is.null(categorical)) {
+      fit <- sem(
+        model,
+        ordered = categorical,
+        data = data,
+        # Requires DWLS for bootstrapping
+        estimator = "DWLS",
+        se = "bootstrap",
+        bootstrap = B
+      )
+    } else {
+      fit <- sem(
+        model,
+        data = data,
+        se = "bootstrap",
+        test = test,
+        bootstrap = B
+      )
+    }
   }
 
   # Output
@@ -67,12 +177,9 @@ lavaan.model <- function(y,
     "\n",
     "\n", "PROCESS Model Specifed: ", process.model,
     "\n",
-    "\n",
-    model,
-    "\n",
-    "\n",
-    sep ="")
+    sep = ""
+  )
 
-  # Return results
+  # Return results as lavaan object
   fit
 }
